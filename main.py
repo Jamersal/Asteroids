e986daf11b38b2e153eff4afbbe98e846608428b
James Amyouny <jamesamyouny@gmail.com>
2026-04-07 12:17:24 -0400

    fixed bug

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
 
         log_state()
         for event in pygame.event.get():
-            if event.type == pygame.QUIT():
+            if event.type == pygame.QUIT:
                 return 
                 
 
