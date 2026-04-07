b75c93398896a02ced184acf6ba34656accef3af
James Amyouny <jamesamyouny@gmail.com>
2026-04-07 12:34:05 -0400

    added fps limit

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True: 
         screen.fill("black")
         pygame.display.flip()
@@ -15,6 +19,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+        dt = clock.tick(60) / 1000
                 
 
 
